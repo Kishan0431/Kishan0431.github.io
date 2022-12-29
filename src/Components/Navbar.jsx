fb3e838ab2cc7d5660aad39edc43274094c5949a
Kishan0431 <Kishanvjoshi1998@gmail.com>
2022-12-30 01:08:59 +0530

    navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {Link} from "react-router-dom"
+import styles from "../Styles/Navbar.module.css";
+
+export const Navbar = () => {
+  return <div className={styles.navbar_container}>
+    <h2>Portfolio</h2>
+        {/* <Link to={"/"}>Home</Link>
+        <Link to={"/AboutMe"}>About Me</Link> */}
+    </div>
+  
+}
