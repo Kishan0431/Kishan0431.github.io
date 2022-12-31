c0acd37b5e208e452003e06f333d96168b036b96
Kishan0431 <Kishanvjoshi1998@gmail.com>
2022-12-31 16:25:31 +0530

    portfolio

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,8 +5,7 @@ import styles from "../Styles/Navbar.module.css";
 export const Navbar = () => {
   return <div className={styles.navbar_container}>
     <h2>Portfolio</h2>
-        {/* <Link to={"/"}>Home</Link>
-        <Link to={"/AboutMe"}>About Me</Link> */}
+        
     </div>
   
 }
