b342b2f6c00c29958b80270a1b7f75dfc3d6ea4d
Kishan0431 <Kishanvjoshi1998@gmail.com>
2023-01-09 20:15:26 +0530

    project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Profile } from './Components/Profile';
 import { Navbar } from './Components/Navbar';
 import { About } from './Components/About';
 import { ChakraProvider } from '@chakra-ui/react'
+import { Project } from './Components/Project';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Profile />
       <About />
+      <Project />
       </ChakraProvider>
       
     </div>
---
+++ src/Components/Project.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../Styles/Project.module.css";
+
+
+export const Project = () => {
+  return (
+    <div className={styles.project_div}>
+        <h1>Projects</h1>
+    </div>
+  )
+}
