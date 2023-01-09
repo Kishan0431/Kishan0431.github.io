1127cd75143568931317116af48faf33be53e68d
Kishan0431 <Kishanvjoshi1998@gmail.com>
2023-01-10 01:31:57 +0530

    project update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,6 +4,9 @@ import styles from "../Styles/About.module.css";
 export const About = () => {
   return <div>
     <div id={styles.About_container}>
+{/* eslint-disable-next-line */}
+    <marquee  direction="left">नमस्ते! I'm Full Stack Developer Specialization in MERN stack and Seeking to pursue a career in a challenging environment to hone industry skill.</marquee>
+ 
        <h1>About me</h1>
        <h3>A Motivated aspiring Full Stack Developer with hands-on experience in building
           websites with MERN stack and various web technologies including HTML, CSS,
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from "../Styles/Project.module.css";
 
-
 export const Project = () => {
   return (
     <div className={styles.project_div}>
         <h1>Projects</h1>
+    <div className={styles.project_onetwo}>
+           
+    </div>
+     
     </div>
   )
 }
+
+//https://github.com/Kishan0431/J.Crew/raw/main/Pics/Landing.png
+//https://www37.img2go.com/v2/dl/web7/download-file/3d866d3e-22eb-4c45-a2d9-020f7c7322a6/lumen5.png
