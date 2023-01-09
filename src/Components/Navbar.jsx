2333ad8e85b12de8aa84460e8247fa0026935d9f
Kishan0431 <Kishanvjoshi1998@gmail.com>
2023-01-09 17:43:36 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,6 @@ import { Button} from '@chakra-ui/react'
 export const Navbar = () => {
   return <>
   <div className={styles.navbar_container}>
-    <img id={styles.nav_avatar} src="https://i.ibb.co/rmqSVsP/Image-37.png" alt="avatar" />
     <div className={styles.navbar_div}>
       <h3>Home</h3>
       <h3>About me</h3>
