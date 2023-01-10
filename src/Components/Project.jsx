0eb7ff190fa5e03d1764e27f5bd7d1cd87b66a53
Kishan0431 <Kishanvjoshi1998@gmail.com>
2023-01-10 14:52:12 +0530

    projects added

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -17,20 +17,25 @@ export const Project = () => {
                       borderRadius='lg'
                     />
                     <Stack mt='6' spacing='3'>
-                      <Heading size='md'>J.Crew</Heading>
+                      <Heading size='lg'>J.Crew</Heading>
                       <Text>
                           J.Crew is a clothing company that makes timeless clothes
                           whose style and design changes season to season according
                           to the fashion and new fabrics.
                       </Text>
                     </Stack>
+                    <div className={styles.languages_icon}>
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/html-4387638-3640299.png" alt="html" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/css3-4387639-3640300.png" alt="css" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/javascript-logo-6563586-5453022.png" alt="javascript" />
+                    </div>
                   </CardBody>
                   <CardFooter>
                     <ButtonGroup spacing='2'>
-                      <Button variant='solid' colorScheme='blue'>
-                      <a href="https://kaleidoscopic-torrone-494925.netlify.app/index.html">See this Live</a>
+                      <Button variant='solid'>
+                        <a href="https://kaleidoscopic-torrone-494925.netlify.app/index.html">See this Live</a>
                       </Button>
-                      <Button variant='ghost' colorScheme='blue'>
+                      <Button variant='ghost' className={styles.project_btn}>
                       <a href="https://github.com/Kishan0431/J.Crew">View Code</a>
                       </Button>
                     </ButtonGroup>
@@ -45,27 +50,95 @@ export const Project = () => {
                       borderRadius='lg'
                     />
                     <Stack mt='6' spacing='3'>
-                      <Heading size='md'>Lumen5</Heading>
+                      <Heading size='lg'>Lumen5</Heading>
                       <Text>
                       Lumen5 is a website where a user can easily make videos for content marketing, through leadership and brand awareness in a snap
                       </Text>
                     </Stack>
+                    <div className={styles.languages_icon}>
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/html-4387638-3640299.png" alt="html" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/css3-4387639-3640300.png" alt="css" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/javascript-logo-6563586-5453022.png" alt="javascript" />
+                    </div>
                   </CardBody>
                   <CardFooter>
                     <ButtonGroup spacing='2'>
-                      <Button variant='solid' colorScheme='blue'>
+                      <Button variant='solid'>
                         <a href="https://sanjiv358.github.io/-plucky-throne-4544/">See this Live</a>
                       </Button>
-                      <Button variant='ghost' colorScheme='blue'>
+                      <Button variant='ghost' className={styles.project_btn}>
                         <a href="https://github.com/Kishan0431/-plucky-throne-4544">View Code</a>
                       </Button>
                     </ButtonGroup>
                   </CardFooter>
             </Card>
     </div>
-     
+    <div className={styles.project_onetwo}>
+      {/* ----------------------Idea cart------------------------ */}
+                <Card className={styles.project} maxW='md'>
+                  <CardBody className={styles.card_body}>
+                    <Image className={styles.project_img}
+                      src='https://github.com/fahisayub/ideakart-clone/raw/main/SiteImages/2022-06-18.png '
+                      alt='ideakart'
+                      borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                      <Heading size='lg'>Ideakart</Heading>
+                      <Text>
+                      Ideakart is a first of it's kind marketplace for ideas.it also provides you books at best prices throughout internet where the user can make Novels purchases.
+                      </Text>
+                    </Stack>
+                    <div className={styles.languages_icon}>
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/html-4387638-3640299.png" alt="html" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/css3-4387639-3640300.png" alt="css" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/javascript-logo-6563586-5453022.png" alt="javascript" />
+                    </div>
+                  </CardBody>
+                  <CardFooter>
+                    <ButtonGroup spacing='2'>
+                      <Button variant='solid'>
+                      <a href="https://musical-chaja-ae96fd.netlify.app/">See this Live</a>
+                      </Button>
+                      <Button variant='ghost' className={styles.project_btn}>
+                      <a href="https://github.com/Kishan0431/ossified-temper-8142">View Code</a>
+                      </Button>
+                    </ButtonGroup>
+                  </CardFooter>
+            </Card>
+            {/* ---------------------Lovoda.com---------------------------------- */}
+            <Card className={styles.project} maxW='md'>
+                  <CardBody className={styles.card_body}>
+                    <Image className={styles.project_img}
+                      src='https://user-images.githubusercontent.com/31920143/208379986-804c58ed-d35e-46b5-b9df-071c2c676dc2.png'
+                      alt='lumen5'
+                      borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                      <Heading size='lg'>Lovoda.com</Heading>
+                      <Text>
+                        This website is basically ecommerce website where you can buy female trending jewelries items.
+                      </Text>
+                    </Stack>
+                    <div className={styles.languages_icon}>
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/html-4387638-3640299.png" alt="html" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/css3-4387639-3640300.png" alt="css" />
+                      <img src="https://cdn3d.iconscout.com/3d/free/thumb/javascript-logo-6563586-5453022.png" alt="javascript" />
+                    </div>
+                  </CardBody>
+                  <CardFooter>
+                    <ButtonGroup spacing='2'>
+                      <Button variant='solid'>
+                        <a href="https://lovoda-clone-far-powder-2496.netlify.app/">See this Live</a>
+                      </Button>
+                      <Button variant='ghost' className={styles.project_btn}>
+                        <a href="https://github.com/Kishan0431/Lovoda.com">View Code</a>
+                      </Button>
+                    </ButtonGroup>
+                  </CardFooter>
+            </Card>
+    </div>
     </div>
   )
 }
 
-//https://github.com/Kishan0431/J.Crew/raw/main/Pics/Landing.png
+
